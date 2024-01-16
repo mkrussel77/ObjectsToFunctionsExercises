8ee2fca25722b1b1ffb70a0935dc4ef3a8300487
Michael Krussel <mkrussel@us.ibm.com>
2024-01-16 08:26:24 -0500

    Refactor consoleRpnCalculator

--- src/main/kotlin/com/functional/chapter10/ConsoleRpnCalculator.kt
+++ src/main/kotlin/com/functional/chapter10/ConsoleRpnCalculator.kt
@@ -3,22 +3,27 @@ package com.functional.chapter10
 import com.functional.chapter2.calc
 import com.functional.chapter9.ContextReader
 
+private const val FAREWELL = "Bye!"
+
 fun consoleRpnCalculator(): ContextReader<ConsoleContext, String> = ContextReader { ctx ->
     consoleRpnCalculator(ctx)
-    "Bye!"
+    FAREWELL
 }
 
 tailrec fun consoleRpnCalculator(ctx: ConsoleContext) {
-    val msg = contextPrintln("Write an RPN expression to calculate the result or Q to quit.")
-        .bind { _ -> contextReadln() }
-        .bind { formula ->
-            if (formula == "Q") {
-                contextPrintln("Bye!")
-            } else {
-                contextPrintln("The result is: ${calc(formula)}")
-            }
-        }.runWith(ctx)
-    if (msg != "Bye!") {
+    val msg = nextCalculation().runWith(ctx)
+    if (msg != FAREWELL) {
         consoleRpnCalculator(ctx)
     }
 }
+
+private fun nextCalculation() =
+    contextPrintln("Write an RPN expression to calculate the result or Q to quit.")
+        .bind { _ -> contextReadln() }
+        .bind { formula ->
+            if (formula == "Q") {
+                contextPrintln(FAREWELL)
+            } else {
+                contextPrintln("The result is: ${calc(formula)}")
+            }
+        }
